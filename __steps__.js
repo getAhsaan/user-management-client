0b3938543898ef94eb5536187d33c62e07d44ac2
getahsaan <getahsaan@gamil.com>
2023-05-08 09:33:18 +0600

    step numbering

--- __steps__.js
+++ __steps__.js
@@ -1,9 +1,10 @@
 /*******
- * create a post api on the server side
- * client side send data via post
- * set fetch method inside the fetch options (second parameter)
- * option will have three things: method
- * option will have headers: 'content-type' : 'application/json'
- * don't forget to send data by JSON.Stringify in the body
- * on the server side: don't forget to use express.json() middleware
+ * 1. create a post api on the server side
+ * 2. client side send data via post
+ * 3. set fetch method inside the fetch options (second parameter)
+ * 4. option will have three things: method
+ * 5. option will have headers: 'content-type' : 'application/json'
+ * 6. don't forget to send data by JSON.Stringify in the body
+ * 7. on the server side: don't forget to use express.json() middleware
+ * 
  */
